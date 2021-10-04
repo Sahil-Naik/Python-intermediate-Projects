39db9ecb671e00b122f21bafd9998d9d62be52e4
Sahil Naik <81952895+Sahil-Naik@users.noreply.github.com>
2021-10-04 07:41:45 +0530

    Update HandWritten.py

--- HandWritten.py
+++ HandWritten.py
@@ -1,3 +1,6 @@
+# This program is used to convert text into hand written img.
+# Enter a text and wait for few seconds for the program to process it.
+
 import pywhatkit
 u_text = input("Enter some text : ")
 pywhatkit.text_to_handwriting(u_text, rgb=[0,0,255])
