dd7090ffbb5dbe33b3155436b54e845101b32079
Sahil Naik <81952895+Sahil-Naik@users.noreply.github.com>
2022-02-21 13:03:30 +0530

    Update HandWritten.py

--- HandWritten.py
+++ HandWritten.py
@@ -2,5 +2,5 @@
 # Enter a text and wait for few seconds for the program to process it.
 
 import pywhatkit
-u_text = input("Enter some text : ")
+u_text = input("Enter some text: ")
 pywhatkit.text_to_handwriting(u_text, rgb=[0,0,255])
